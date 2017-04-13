999674c959bb15d990ead79b473f530485e0ecad
LPeter1997 <lenkefi.peti@gmail.com>
2017-04-13 21:28:01 +0200

    Basic semantic checker

--- YoakkeCompilerBranch2/src/semantics/semantic_checker.h
+++ YoakkeCompilerBranch2/src/semantics/semantic_checker.h
@@ -1,7 +1,21 @@
 #pragma once
 
-namespace yk {
-	class semantic_checker {
+#include "symbol_table.h"
 
+namespace yk {
+	class stmt;
+	class expr;
+	class pattern;
+	class type_desc;
+	class type_symbol;
+
+	class semantic_checker {
+	private:
+		symbol_table m_Table;
+
+	public:
+		void check_stmt(yshared_ptr<stmt> st);
+		void check_expr(yshared_ptr<expr> ex);
+		void check_type(yshared_ptr<type_desc> ty);
 	};
 }
