bb5de05e0e1ee58b650a05985778de82c5dbfb5d
LPeter1997 <lenkefi.peti@gmail.com>
2017-05-20 20:56:49 +0200

    Removed old semantic check

--- YoakkeCompilerBranch2/src/ast/ast.h
+++ YoakkeCompilerBranch2/src/ast/ast.h
@@ -1,9 +1,5 @@
 #pragma once
 
-/*
-This file is just a collection of the different AST nodes for simple inclusion.
-*/
-
 #include "stmt.h"
 #include "expr.h"
 #include "ty_expr.h"
--- YoakkeCompilerBranch2/src/semantics/old/symbol.cpp
+++
@@ -1,9 +0,0 @@
-#include "symbol.h"
-
-namespace yk {
-	symbol::symbol(ystr const& id)
-		: Identifier(id) {
-	}
-
-	symbol::~symbol() { }
-}
