fbe3d9ab924320ea8790952b253843dd744cb99b
LPeter1997 <lenkefi.peti@gmail.com>
2017-07-19 21:32:14 +0200

    Commented token input

--- YCompiler/src/syntax/token_input.h
+++ YCompiler/src/syntax/token_input.h
@@ -18,13 +18,33 @@ struct token;
 
 struct token_input {
 public:
+	/**
+	 * Creates a token input at the beginning of the lexer.
+	 * @param lex The lexer to read from.
+	 */
 	token_input(lexer& lex);
 
 public:
+	/**
+	 * Returns the token at the position this instance points at.
+	 * @return The pointed token.
+	 */
 	token& head();
+	
+	/**
+	 * Returns the token input proceeding this one.
+	 * @return The token input containing the rest of the tokens.
+	 */
 	token_input tail();
 
 private:
+	/**
+	 * Internally constructs a token input at the given position.
+	 * Used by tail().
+	 * @see tail().
+	 * @param lex The lexer to get the input from.
+	 * @param idx The position in the lexer.
+	 */
 	token_input(lexer& lex, ysize idx);
 
 private:
