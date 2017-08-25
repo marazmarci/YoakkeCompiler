b32bfc143d7338a7969f4a370311f61e5393b31e
LPeter1997 <lenkefi.peti@gmail.com>
2017-08-25 14:09:01 +0200

    Phase1 done, untested

--- YCompiler/src/semantics/checker.cpp
+++ YCompiler/src/semantics/checker.cpp
@@ -29,6 +29,8 @@ yopt<semantic_err> checker::check_program(yvec<AST_stmt*>& prg) {
 	}
 }
 
+// PHASE 1 ////////////////////////////////////////////////////////////////////
+
 yopt<semantic_err> checker::phase1(AST_stmt* st) {
 	switch (st->Ty) {
 	case AST_stmt_t::ConstDecl: UNIMPLEMENTED;
@@ -81,8 +83,130 @@ yopt<semantic_err> checker::phase1(AST_stmt* st) {
 }
 
 yopt<semantic_err> checker::phase1(AST_expr* ex) {
+	switch (ex->Ty) {
+	case AST_expr_t::Block: {
+		auto expr = (AST_block_expr*)ex;
+		expr->Scope = SymTab.push_scope(!expr->AsStatement);
+		for (auto& st : expr->Statements) {
+			if (auto err = phase1(st)) {
+				return err;
+			}
+		}
+		if (expr->Value) {
+			if (auto err = phase1(*expr->Value)) {
+				return err;
+			}
+		}
+		SymTab.pop_scope();
+		return {};
+	}
 
+	case AST_expr_t::Call: {
+		auto expr = (AST_call_expr*)ex;
+		if (auto err = phase1(expr->Func)) {
+			return err;
+		}
+		for (auto& param : expr->Params) {
+			if (auto err = phase1(param)) {
+				return err;
+			}
+		}
+		return {};
+	}
+
+	case AST_expr_t::Fn: {
+		auto expr = (AST_fn_expr*)ex;
+		expr->Scope = SymTab.push_scope(true);
+		if (auto err = phase1(expr->Body)) {
+			return err;
+		}
+		SymTab.pop_scope();
+		return {};
+	}
+
+	case AST_expr_t::If: {
+		auto expr = (AST_if_expr*)ex;
+		if (auto err = phase1(expr->Condition)) {
+			return err;
+		}
+		// Set up blocks
+		if (expr->AsStatement) {
+			expr->Then->AsStatement = true;
+			if (expr->Else) {
+				(*expr->Else)->AsStatement = true;
+			}
+		}
+		if (auto err = phase1(expr->Then)) {
+			return err;
+		}
+		if (expr->Else) {
+			if (auto err = phase1(*expr->Else)) {
+				return err;
+			}
+		}
+		return {};
+	}
+
+	case AST_expr_t::Let: {
+		auto expr = (AST_let_expr*)ex;
+		if (expr->Value) {
+			if (auto err = phase1(*expr->Value)) {
+				return err;
+			}
+		}
+		return {};
+	}
+
+	case AST_expr_t::List: {
+		auto expr = (AST_list_expr*)ex;
+		for (auto& exp : expr->Elements) {
+			if (auto err = phase1(exp)) {
+				return err;
+			}
+		}
+		return {};
+	}
+
+	case AST_expr_t::Pre: {
+		auto expr = (AST_pre_expr*)ex;
+		if (auto err = phase1(expr->Subexpr)) {
+			return err;
+		}
+		return {};
+	}
+
+	case AST_expr_t::Bin: {
+		auto expr = (AST_bin_expr*)ex;
+		if (auto err = phase1(expr->Left)) {
+			return err;
+		}
+		if (auto err = phase1(expr->Right)) {
+			return err;
+		}
+		return {};
+	}
+
+	case AST_expr_t::Post: {
+		auto expr = (AST_post_expr*)ex;
+		if (auto err = phase1(expr->Subexpr)) {
+			return err;
+		}
+		return {};
+	}
+
+	case AST_expr_t::Ident:
+	case AST_expr_t::IntLit:
+	case AST_expr_t::RealLit: {
+		return {};
+	}
+
+	default: UNIMPLEMENTED;
+	}
+
+	UNREACHABLE;
 }
+
+// PHASE 2 ////////////////////////////////////////////////////////////////////
 
 void checker::print_shadow(ystr const& report, ystr const& kind, ystr const& name,
 	yopt<semantic_pos> defpos, semantic_pos const& redefpos) {
