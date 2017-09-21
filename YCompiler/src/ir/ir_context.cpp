ebc5a7004a03cca71ccbbacc426120257127f2e9
LPeter1997 <lenkefi.peti@gmail.com>
2017-09-21 17:54:16 +0200

    Added decl to context

--- YCompiler/src/ir/ir_context.cpp
+++ YCompiler/src/ir/ir_context.cpp
@@ -1,5 +1,9 @@
 #include "ir_context.h"
 
+void ir_context::add_function_decl(ir_function_decl* decl) {
+	FunctionDecls.push_back(decl);
+}
+
 void ir_context::add_function(ir_function* fn) {
 	Functions.push_back(fn);
 }
--- YCompiler/src/ir/ir_context.h
+++ YCompiler/src/ir/ir_context.h
@@ -3,9 +3,12 @@
 #include "../common.h"
 
 struct ir_function;
+struct ir_function_decl;
 
 struct ir_context {
-	yvec<ir_function*> Functions;
+	yvec<ir_function_decl*> FunctionDecls;
+	yvec<ir_function*>		Functions;
 
+	void add_function_decl(ir_function_decl* decl);
 	void add_function(ir_function* fn);
 };
